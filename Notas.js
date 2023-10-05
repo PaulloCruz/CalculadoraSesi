c5370cc6c8a0301b3fa7f5a3440299cb138097df
PaulloCruz <paullo.cruz@hotmail.com>
2023-10-05 08:44:04 -0300

    Update Notas.js

--- Notas.js
+++ Notas.js
@@ -5,7 +5,7 @@ document.getElementById('calculate').addEventListener('click', function(){
     const av3 =document.getElementById('av3').value;
     const avt =document.getElementById('avt').value;
 
-    if(av1>10||av1<0||av2>10|av2<0||av3>10||av3<0||avt>10||avt<0){
+    if(av1>10 || av1<0 || av2>10 || av2<0 || av3>10 || av3<0 || avt>10 || avt<0){
     alert("Digite um valor de 0 a 10")
     location.reload()
     }
