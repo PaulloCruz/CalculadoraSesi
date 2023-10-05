a48954964669daf65c1bfdc153f7de8d62da6bc8
PaulloCruz <paullo.cruz@hotmail.com>
2023-10-05 08:39:06 -0300

    Update Notas.js

--- Notas.js
+++ Notas.js
@@ -5,6 +5,11 @@ document.getElementById('calculate').addEventListener('click', function(){
     const av3 =document.getElementById('av3').value;
     const avt =document.getElementById('avt').value;
 
+    if(av1>10||av1<0||av2>10|av2<0||av3>10||av3<0||avt>10||avt<0){
+    alert("Digite um valor de 0 a 10)
+    location.reload()
+    }
+
     const total= ((av1*25)+(av2*25)+(av3*20)+(avt*30))/100
    
     document.getElementById('total').innerHTML = (total)
